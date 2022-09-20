a7f8a325db30cd85b098d687bc301c3bfd762460
stanstevo <stevemth@gmail.com>
2022-09-20 17:27:27 +0300

    initial commit

---
+++ app/src/main/java/com/unya/plantdiary/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.unya.plantdiary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.unya.plantdiary.ui.theme.PlantDiaryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlantDiaryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android & Goodbye")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PlantDiaryTheme {
+        Greeting("Android and Good bye")
+    }
+}
